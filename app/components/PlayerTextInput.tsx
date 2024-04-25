93049d501e81d8e28332ec7ee0eb989d35dc5a93
Darth Varg <cbvargas123@gmail.com>
2024-04-25 11:01:35 -0700

    inputs ready for backend

--- app/components/PlayerTextInput.tsx
+++ app/components/PlayerTextInput.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+'use client'
+import { useState } from 'react'
+
+interface State {
+  userInputText: string
+}
 
 const PlayerTextInput: React.FC = () => {
+  const initialState: State = {
+    userInputText: '',
+  }
+  const [state, setState] = useState<State>(initialState)
+
+  const handleSubmitText = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setState(initialState)
+  }
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setState((prevState: State) => ({
+      ...prevState,
+      userInputText: e.target.value,
+    }))
+  }
+
   return (
-    <div>
-      <input type="text" placeholder="Message your DM" />
-    </div>
+    <form onSubmit={handleSubmitText}>
+      <textarea
+        className="text-black"
+        placeholder="Message your DM"
+        onChange={handleInputChange}
+        value={state.userInputText}
+      />
+      {/* to do: change to icon */}
+      <button type="submit">Submit</button>
+    </form>
   )
 }
 
